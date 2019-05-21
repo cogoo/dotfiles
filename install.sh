8a32a465cba785586043586155d0aa4e16af14da
Colin Ogoo <ogoo.colin@gmail.com>
2019-05-21 09:28:34 +0100

    update: remove prompt

--- install.sh
+++ install.sh
@@ -13,13 +13,5 @@ function doIt() {
 	source ~/.zshrc;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+doIt;
 unset doIt;
