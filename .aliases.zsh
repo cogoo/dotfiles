2ce88a92eb41861ecc63c1083f08e4eade37bbb0
Colin Ogoo <ogoo.colin@gmail.com>
2019-07-30 21:36:14 +0100

    feat: 🎸 update config; tmux, alias
    add tmux conf

--- .aliases.zsh
+++ .aliases.zsh
@@ -25,4 +25,4 @@ alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 alias get-app="brew cask install"
-
+alias nx-create="~/.dotfiles/scripts/nx.zsh"
