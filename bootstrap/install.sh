7bafe105c1e3396464d2931dc97037b90b71dc67
Colin Ogoo <colin.ogoo@bynd.com>
2021-02-06 13:04:07 +0000

    refactor: linting

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 #set -ex
 
@@ -8,218 +8,214 @@ echo "🚀 Starting bootstrap"
 sudo -v
 
 GEMS=(
-	rest-client
+  rest-client
 )
 
 install_homebrew() {
-	if test $(which brew); then
-		echo "😊 Homebrew installed..."
-		return 0
-	fi
+  if test "$(which brew)"; then
+    echo "😊 Homebrew installed..."
+    return 0
+  fi
 
-	echo "😎 Installing homebrew..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "😎 Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 }
 
 update_homebrew() {
-	brew update
+  brew update
 }
 
 install_nvm() {
-	echo "😎 Installing NVM..."
-	export NVM_DIR="$HOME/.nvm" && (
-		git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
-		cd "$NVM_DIR"
-		git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
-	) && \. "$NVM_DIR/nvm.sh"
+  echo "😎 Installing NVM..."
+  export NVM_DIR="$HOME/.nvm" && (
+    git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+    cd "$NVM_DIR"
+    git checkout $(git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
+  ) && \. "$NVM_DIR/nvm.sh"
 }
 
 setup_node() {
-	echo "😎 Setting up Node w/ NVM ..."
-	if [ -z "$NVM_DIR" ]; then
-		# NVM_DIR is undefined, so it's not been initialised
-		NVM_DIR="$HOME/.nvm"
-	fi
+  echo "😎 Setting up Node w/ NVM ..."
+  if [ -z "$NVM_DIR" ]; then
+    # NVM_DIR is undefined, so it's not been initialised
+    NVM_DIR="$HOME/.nvm"
+  fi
 
-	. "$NVM_DIR/nvm.sh"
+  . "$NVM_DIR/nvm.sh"
 
-	nvm install lts/dubnium
-	nvm alias default lts/dubnium
+  nvm install lts/dubnium
+  nvm alias default lts/dubnium
 }
 
 install_rvm() {
-	echo "😎 Installing RVM..."
-	ruby -e "$(curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles)"
+  echo "😎 Installing RVM..."
+  ruby -e "$(curl -sSL https://get.rvm.io | bash -s stable --ruby --auto-dotfiles)"
 }
 
 cleanup_homebrew() {
-	echo "🛀🏽 Cleaning up..."
-	brew cleanup
+  echo "🛀🏽 Cleaning up..."
+  brew cleanup
 }
 
 install_packages() {
-	local PCKGS_FILE="$DOTFILES/bootstrap/brew_packages"
+  local PCKGS_FILE="$DOTFILES/bootstrap/brew_packages"
 
-	if [ -f "$PCKGS_FILE" ]; then
-		echo "📦 Installing packages..."
-		while IFS= read -r pckg
-		do
-		command -v "${pckg}" &>/dev/null && continue
+  if [ -f "$PCKGS_FILE" ]; then
+    echo "📦 Installing packages..."
+    while IFS= read -r pckg; do
+      command -v "${pckg}" &>/dev/null && continue
 
-		echo "📦 Installing: ${pckg}"
-		brew install ${pckg}
-		done < "$PCKGS_FILE"
-	fi
+      echo "📦 Installing: ${pckg}"
+      brew install "${pckg}"
+    done <"$PCKGS_FILE"
+  fi
 }
 
 install_apps() {
-	brew tap homebrew/cask-versions
-	local CASKS_FILE="$DOTFILES/bootstrap/brew_casks"
+  brew tap homebrew/cask-versions
+  local CASKS_FILE="$DOTFILES/bootstrap/brew_casks"
 
-	if [ -f "$CASKS_FILE" ]; then
-		while IFS= read -r cask
-		do
-		#TODO: check if app already exists
-		echo "📦 Installing ${cask}"
-		brew cask install ${cask}
-		done < "$CASKS_FILE"
-	fi
+  if [ -f "$CASKS_FILE" ]; then
+    while IFS= read -r cask; do
+      #TODO: check if app already exists
+      echo "📦 Installing ${cask}"
+      brew cask install "${cask}"
+    done <"$CASKS_FILE"
+  fi
 }
 
 install_fonts() {
-	echo "📦 Installing fonts..."
-	brew tap homebrew/cask-fonts
-	local FONTS_FILE="$DOTFILES/bootstrap/brew_fonts"
+  echo "📦 Installing fonts..."
+  brew tap homebrew/cask-fonts
+  local FONTS_FILE="$DOTFILES/bootstrap/brew_fonts"
 
-	if [ -f "$FONTS_FILE" ]; then
-		while IFS= read -r font
-		do
-		echo "📦 Installing ${font}"
-		brew cask install ${font}
-		done < "$FONTS_FILE"
-	fi
+  if [ -f "$FONTS_FILE" ]; then
+    while IFS= read -r font; do
+      echo "📦 Installing ${font}"
+      brew cask install "${font}"
+    done <"$FONTS_FILE"
+  fi
 }
 
 install_npm_packages() {
-	echo "📦 Installing Global NPM packages..."
-	local NPM_GLOBALS="$DOTFILES/bootstrap/npm_globals"
+  echo "📦 Installing Global NPM packages..."
+  local NPM_GLOBALS="$DOTFILES/bootstrap/npm_globals"
 
-	if [ -f "$NPM_GLOBALS" ]; then
-		# check if package exists
-		while IFS= read -r npm_pckg
-		do
-		npm i -g ${npm_pckg}
-		done < "$NPM_GLOBALS"
+  if [ -f "$NPM_GLOBALS" ]; then
+    # check if package exists
+    while IFS= read -r npm_pckg; do
+      npm i -g "${npm_pckg}"
+    done <"$NPM_GLOBALS"
 
-		# Set-up NVM globals
-		cp "$NPM_GLOBALS" "$NVM_DIR/default-packages"
-	fi
+    # Set-up NVM globals
+    cp "$NPM_GLOBALS" "$NVM_DIR/default-packages"
+  fi
 }
 
 install_antibody() {
-	brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
-	brew install antibody
+  brew tap | grep -q 'getantibody/tap' || brew tap getantibody/tap
+  brew install antibody
 }
 
 install_tmuxinator() {
-	# Req: >= ruby@2.4.6
-	echo "📦 Installing TMUXinator..."
-	gem install tmuxinator
+  # Req: >= ruby@2.4.6
+  echo "📦 Installing TMUXinator..."
+  gem install tmuxinator
 }
 
 setup_project_folders() {
-	GITHUB_DIR="$HOME/LocalHost/GitHub"
-	GLOBAL_DIR="$HOME/LocalHost/Global"
-	BYND_DIR="$HOME/LocalHost/BYND"
+  GITHUB_DIR="$HOME/LocalHost/GitHub"
+  GLOBAL_DIR="$HOME/LocalHost/Global"
+  BYND_DIR="$HOME/LocalHost/BYND"
 
-	echo "Creating folder structure..."
+  echo "Creating folder structure..."
 
-	if ! [ -d "$GITHUB_DIR" ]; then
-		mkdir -p "$GITHUB_DIR"
-	fi
+  if ! [ -d "$GITHUB_DIR" ]; then
+    mkdir -p "$GITHUB_DIR"
+  fi
 
-	if ! [ -d "$GLOBAL_DIR" ]; then
-		mkdir -p "$GLOBAL_DIR"
-	fi
+  if ! [ -d "$GLOBAL_DIR" ]; then
+    mkdir -p "$GLOBAL_DIR"
+  fi
 
-	if ! [ -d "$BYND_DIR" ]; then
-		mkdir -p "$BYND_DIR"
-	fi
+  if ! [ -d "$BYND_DIR" ]; then
+    mkdir -p "$BYND_DIR"
+  fi
 }
 
-install_flutter(){
-	cd "$GLOBAL_DIR"
-	git clone https://github.com/flutter/flutter.git
+install_flutter() {
+  cd "$GLOBAL_DIR" || exit
+  git clone https://github.com/flutter/flutter.git
 
-	# Install cocoapods
-	gem install cocoapods
+  # Install cocoapods
+  gem install cocoapods
 }
 
 read -ep "Install homebrew? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_homebrew
+  install_homebrew
 fi
 
 read -ep "Update homebrew? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	update_homebrew
+  update_homebrew
 fi
 
 read -ep "Install RVM? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_rvm
+  install_rvm
 fi
 
 read -ep "Install Packages? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_packages
+  install_packages
 fi
 
 read -ep "Install Apps? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_apps
+  install_apps
 fi
 
 read -ep "Install Fonts? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_fonts
+  install_fonts
 fi
 
 read -ep "Install NVM? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_nvm
+  install_nvm
 fi
 
 read -ep "Set-up Node? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	setup_node
+  setup_node
 fi
 
 read -ep "Install NPM packages? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_npm_packages
+  install_npm_packages
 fi
 
 read -ep "Install Tmuxinator? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_tmuxinator
+  install_tmuxinator
 fi
 
 read -ep "Install Antibody? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_antibody
+  install_antibody
 fi
 
 read -ep "Setup project folders? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	setup_project_folders
+  setup_project_folders
 fi
 
 read -ep "Install Flutter? (y/n) " ANSWER
 if [ "$ANSWER" = "y" ]; then
-	install_flutter
+  install_flutter
 fi
 
 #cleanup_homebrew
 
-echo "\\n🦁  Bootstrapping complete \\n"
+printf "\\n🦁  Bootstrapping complete \\n"
--- vscode/install.sh
+++ vscode/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 EXTENSIONS_FILE="$HOME/.dotfiles/vscode/extensions"
 SETTINGS_FILE="$HOME/.dotfiles/vscode/settings.json"
@@ -6,24 +6,23 @@ KEYBINDINGS_FILE="$HOME/.dotfiles/vscode/keybindings.json"
 SNIPPETS_DIR="$HOME/.dotfiles/vscode/snippets"
 
 if [ -f "$EXTENSIONS_FILE" ]; then
-	echo "🎙  Installing VS code extensions \\n"
-	while IFS= read -r extension
-	do
-	code --install-extension "$extension"
-	done < "$EXTENSIONS_FILE"
+  printf "🎙  Installing VS code extensions \n"
+  while IFS= read -r extension; do
+    code --install-extension "$extension"
+  done <"$EXTENSIONS_FILE"
 fi
 
 if [ -f "$SETTINGS_FILE" ]; then
-	echo "🎙  Copying VS code settings  \\n"
-	cp "$SETTINGS_FILE" "$HOME/Library/Application Support/Code/User/settings.json"
+  printf "🎙  Copying VS code settings  \n"
+  cp "$SETTINGS_FILE" "$HOME/Library/Application Support/Code/User/settings.json"
 fi
 
 if [ -f "$KEYBINDINGS_FILE" ]; then
-	echo "🎙  Copying VS code keybindings  \\n"
-	cp "$KEYBINDINGS_FILE" "$HOME/Library/Application Support/Code/User/keybindings.json"
+  printf "🎙  Copying VS code keybindings  \n"
+  cp "$KEYBINDINGS_FILE" "$HOME/Library/Application Support/Code/User/keybindings.json"
 fi
 
 if [ -d "$SNIPPETS_DIR" ]; then
-	echo "🎙  Copying VS code snippets  \\n"
-	cp -R "$SNIPPETS_DIR" "$HOME/Library/Application Support/Code/User"
+  printf "🎙  Copying VS code snippets  \n"
+  cp -R "$SNIPPETS_DIR" "$HOME/Library/Application Support/Code/User"
 fi
