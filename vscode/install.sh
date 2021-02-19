8c44796f95330ed9a940764283ca25b9dd24e9af
Colin Ogoo <ogoo.colin@gmail.com>
2021-02-19 10:51:01 +0000

    refactor: remove unused packages
    stylistic refactors

--- vscode/install.sh
+++ vscode/install.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
-
-EXTENSIONS_FILE="$HOME/.dotfiles/vscode/extensions"
-SETTINGS_FILE="$HOME/.dotfiles/vscode/settings.json"
-KEYBINDINGS_FILE="$HOME/.dotfiles/vscode/keybindings.json"
 SNIPPETS_DIR="$HOME/.dotfiles/vscode/snippets"
-
-if [ -f "$EXTENSIONS_FILE" ]; then
-  printf "🎙  Installing VS code extensions \n"
-  while IFS= read -r extension; do
-    code --install-extension "$extension"
-  done <"$EXTENSIONS_FILE"
-fi
-
-if [ -f "$SETTINGS_FILE" ]; then
-  printf "🎙  Copying VS code settings  \n"
-  cp "$SETTINGS_FILE" "$HOME/Library/Application Support/Code/User/settings.json"
-fi
-
-if [ -f "$KEYBINDINGS_FILE" ]; then
-  printf "🎙  Copying VS code keybindings  \n"
-  cp "$KEYBINDINGS_FILE" "$HOME/Library/Application Support/Code/User/keybindings.json"
-fi
 
 if [ -d "$SNIPPETS_DIR" ]; then
   printf "🎙  Copying VS code snippets  \n"
